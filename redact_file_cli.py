a32d7f74064b6c0fdc567dab7c69a9ffc619e23b
shaurya1negi <shaurya.23bai11098@vitbhopal.ac.in>
2025-10-09 18:59:38 +0530

    minor improvements

--- redact_file_cli.py
+++ redact_file_cli.py
@@ -5,7 +5,7 @@ current_dir = os.path.dirname(os.path.abspath(__file__))
 parent_dir = os.path.dirname(current_dir)
 if parent_dir not in sys.path:
     sys.path.insert(0, parent_dir)
-from PII_modules import PIIPipeline
+from PIIshield import PIIPipeline
 
 class PDFRedactionHandler:
     def __init__(self):
